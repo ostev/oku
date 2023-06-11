46686cff929debaa101fe3bbfb9db1d23e9934ea
Oscar Stevens <ostev@outlook.com.au>
2023-06-11 10:17:53 +1000

    Experiment with fxaa

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -2,11 +2,13 @@ import * as Three from "three"
 
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js"
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js"
+import { FXAAShader } from "three/addons/shaders/FXAAShader.js"
 
 import { Player } from "./Player"
 import { OutlinePass } from "./render/OutlinePass"
 
 import paperTextureUrl from "./paper2k.png?url"
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js"
 
 export class Renderer {
     camera: Three.OrthographicCamera
@@ -22,6 +24,7 @@ export class Renderer {
     composer: EffectComposer
 
     outlinePass: OutlinePass
+    fxaaPass: ShaderPass
 
     constructor() {
         this.camera = new Three.OrthographicCamera()
@@ -39,8 +42,7 @@ export class Renderer {
         this.scene.add(this.sun)
 
         this.renderer = new Three.WebGLRenderer({
-            powerPreference: "high-performance",
-            antialias: true
+            powerPreference: "high-performance"
         })
         // this.renderer.setAnimationLoop(this.animation)
 
@@ -56,6 +58,9 @@ export class Renderer {
 
         this.outlinePass = new OutlinePass(this.scene, this.camera, 1, 1)
         this.composer.addPass(this.outlinePass)
+
+        this.fxaaPass = new ShaderPass(FXAAShader)
+        // this.composer.addPass(this.fxaaPass)
     }
 
     // load = async () => {
@@ -72,6 +77,14 @@ export class Renderer {
     }
 
     setSize = (width: number, height: number) => {
+        const pixelRatio = window.devicePixelRatio
+
+        this.renderer.setSize(width, height)
+        this.renderer.setPixelRatio(pixelRatio)
+
+        this.composer.setSize(width, height)
+        this.composer.setPixelRatio(pixelRatio)
+
         const halfWidth = (this.orthographicScale * width) / 2
         const halfHeight = (this.orthographicScale * height) / 2
 
@@ -83,9 +96,10 @@ export class Renderer {
         this.camera.updateProjectionMatrix()
 
         this.outlinePass.setSize(width, height)
-
-        this.renderer.setSize(width, height)
-        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.fxaaPass.material.uniforms["resolution"].value.x =
+            1 / (width * pixelRatio)
+        this.fxaaPass.material.uniforms["resolution"].value.y =
+            1 / (height * pixelRatio)
     }
 
     animate = (delta: number) => {
