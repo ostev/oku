e43c9cc17bfe90c68c0cb2459af33804b4083df2
Oscar Stevens <ostev@outlook.com.au>
2023-07-26 14:28:16 +1000

    Add new sounds

--- src/renderer/audio/sounds.ts
+++ src/renderer/audio/sounds.ts
@@ -5,6 +5,8 @@ import itemHoverUrl from "./sounds/item-hover.wav"
 import okayUrl from "./sounds/okay.wav"
 import pickUpUrl from "./sounds/pick-up.wav"
 import placeDownUrl from "./sounds/place-down.wav"
+import concertUrl from "./sounds/concert.wav"
+import menuUrl from "./sounds/menu.wav"
 
 export const alert = new Tone.Player(alertUrl).toDestination()
 
@@ -17,3 +19,5 @@ itemHover.volume.value = -10
 export const okay = new Tone.Player(okayUrl).toDestination()
 export const pickUp = new Tone.Player(pickUpUrl).toDestination()
 export const placeDown = new Tone.Player(placeDownUrl).toDestination()
+export const concert = new Tone.Player(concertUrl).toDestination()
+export const menu = new Tone.Player(menuUrl).toDestination()
