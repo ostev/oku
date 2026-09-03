4011b9319440c9f4e01b0bd411041048f219022d
ostev <82244060+ostev@users.noreply.github.com>
2026-09-03 13:21:46 +1000

    Clean up comments

--- src/main.js
+++ src/main.js
@@ -1,59 +1,58 @@
-const { app, BrowserWindow, session } = require("electron")
-const path = require("path")
+const { app, BrowserWindow, session } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
-    // Create the browser window.
+    // Create the browser window
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
-    })
+    });
 
     // and load the index.html of the app.
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
     } else {
         mainWindow.loadFile(
             path.join(
                 __dirname,
                 `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`
             )
-        )
+        );
     }
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
-}
+    // Open the DevTools
+    mainWindow.webContents.openDevTools();
+};
 
 // Allow autoplay
-app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required")
+app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on("activate", () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+
 
 session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     callback({
@@ -61,5 +60,5 @@ session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
             ...details.responseHeaders,
             "Content-Security-Policy": ["default-src 'none'"],
         },
-    })
-})
+    });
+});
